2afa13555c3285af795eda74df1a76c959368257
Nico Hämäläinen <nico@sizeof.io>
2015-02-18 18:42:34 +0200

    Project basis

---
+++ bedrock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bedrock
+//
+//  Created by Nico Hämäläinen on 18/02/15.
+//  Copyright (c) 2015 sizeof.io. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
